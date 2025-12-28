1730fe979f487b0fbd16983873bc5a15be753e7e
redyf <mateusalvespereira7@gmail.com>
2025-12-28 17:00:10 -0300

    feat: Cria requisição para as classes modeladas

--- Program.cs
+++ Program.cs
@@ -1,16 +1,30 @@
 ﻿using System.Text.Json;
-using ScreenSound_04;
+using ScreenSound_04.Models;
 
-using (HttpClient client = new HttpClient())
+internal class Program
 {
-    try
+    private static async Task Main(string[] args)
     {
-        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        musicas.ForEach(music => music.ExibirDetalhesDaMusica());
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine($"Fetching failed: {e.Message}");
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+                var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+                musicas.ForEach(music => music.ExibirDetalhesDaMusica());
+
+                string response = await client.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/TopMovies.json");
+                var filmes = JsonSerializer.Deserialize<List<Filme>>(response)!;
+                filmes.ForEach(filme => filme.ExibirDetalhes());
+
+                string respostaPais = await client.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/Paises.json");
+                var paises = JsonSerializer.Deserialize<List<Pais>>(respostaPais)!;
+                paises.ForEach(pais => pais.ExibirDetalhes());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Fetching failed: {e.Message}");
+            }
+        }
     }
 }
