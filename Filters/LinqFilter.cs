c52ee0d0f91cd5d8c4c246a09be5296472170fbb
redyf <mateusalvespereira7@gmail.com>
2025-12-29 20:55:10 -0300

    feat: add new filters

--- Filters/LinqFilter.cs
+++ Filters/LinqFilter.cs
@@ -35,4 +35,25 @@ public class LinqFilter
         Console.WriteLine($"Os números únicos na lista são: ");
         numerosUnicos.ForEach(numero => Console.WriteLine(numero));
     }
+
+    public static void ListaDeLivrosPublicadosApósXAno(List<Livro> livros, int ano)
+    {
+        var anoDePublicacao = ano;
+        var livrosPublicadosAposXAno = livros.Where(livro => livro.AnoDePublicacao > anoDePublicacao).OrderBy(livro => livro.Titulo).Select(livro => livro.Titulo).ToList();
+
+        Console.WriteLine("Esta é a lista de livros publicados após o ano 2000, em ordem alfabética");
+        livrosPublicadosAposXAno.ForEach(livro => Console.WriteLine(livro));
+    }
+
+    public static void PrecoMedioDoProduto(List<Produto> produtos)
+    {
+        var calculaPrecoMedio = produtos.Average(p => p.Preco);
+        Console.WriteLine($"O preço médio do produto é: {calculaPrecoMedio} ");
+    }
+
+    public static void RetornaNumerosPares(List<int> numeros)
+    {
+        var filtraNumerosPares = numeros.Where(num => num % 2 == 0).ToList();
+        filtraNumerosPares.ForEach(num => Console.WriteLine($"O número {num} é par"));
+    }
 }
