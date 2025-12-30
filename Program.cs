5613c44804f2246f1fe60da9c082b3c08a756e4f
redyf <mateusalvespereira7@gmail.com>
2025-12-30 20:07:53 -0300

    chore: filtra as musicas usando o novo metodo

--- Program.cs
+++ Program.cs
@@ -13,19 +13,21 @@ internal class Program
                 string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
                 var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
                 // musicas.ForEach(music => music.ExibirDetalhesDaMusica());
+                musicas[1].ExibirDetalhesDaMusica();
+                LinqFilter.FiltraMusicasPelaTonalidade(musicas, "C#");
 
                 // LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
                 // LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
                 // LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
                 // LinqFilter.FiltrarMusicasDeArtista(musicas, "iann dior");
-                LinqFilter.FiltrarMusicasPorAno(musicas, 2012);
-                var musicasPreferidasDeMateus = new MusicasPreferidas("Mateus");
-                musicasPreferidasDeMateus.AdicionarMusicasFavoritas(musicas[1]);
-                musicasPreferidasDeMateus.AdicionarMusicasFavoritas(musicas[10]);
-                musicasPreferidasDeMateus.AdicionarMusicasFavoritas(musicas[14]);
-
-                musicasPreferidasDeMateus.ExibirMusicasFavoritas();
-                musicasPreferidasDeMateus.GerarArquivoJson();
+                // LinqFilter.FiltrarMusicasPorAno(musicas, 2012);
+                // var musicasPreferidasDeMateus = new MusicasPreferidas("Mateus");
+                // musicasPreferidasDeMateus.AdicionarMusicasFavoritas(musicas[1]);
+                // musicasPreferidasDeMateus.AdicionarMusicasFavoritas(musicas[10]);
+                // musicasPreferidasDeMateus.AdicionarMusicasFavoritas(musicas[14]);
+                //
+                // musicasPreferidasDeMateus.ExibirMusicasFavoritas();
+                // musicasPreferidasDeMateus.GerarArquivoJson();
                 // List<int> numerosComDuplicatas = new List<int> { 1, 1, 1, 2, 3, 4, 5, 5, 5, 6, 6, 7, 8 };
                 // LinqFilter.NumerosUnicosNaLista(numerosComDuplicatas);
                 //
