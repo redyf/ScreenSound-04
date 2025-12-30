36151a84a8a199f30837ccbed394899893abbb13
redyf <mateusalvespereira7@gmail.com>
2025-12-30 20:07:07 -0300

    feat: add tonalidade para modelo Musica

--- Models/Musica.cs
+++ Models/Musica.cs
@@ -4,6 +4,7 @@ namespace ScreenSound_04.Models;
 
 public class Musica
 {
+    private string[] tonalidades = { "C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B" };
     [JsonPropertyName("song")]
     public string Nome { get; set; }
 
@@ -15,6 +16,17 @@ public class Musica
 
     [JsonPropertyName("genre")]
     public string Genero { get; set; }
+
+    [JsonPropertyName("key")]
+    public int Key { get; set; }
+
+    public string Tonalidade
+    {
+        get
+        {
+            return tonalidades[Key];
+        }
+    }
 
     [JsonPropertyName("year")]
     public string? AnoString { get; set; }
@@ -34,5 +46,6 @@ public class Musica
         Console.WriteLine($"Duração: {Duracao / 1000}");
         Console.WriteLine($"Gênero: {Genero}");
         Console.WriteLine($"Ano: {Ano}");
+        Console.WriteLine($"Tonalidade: {Tonalidade}");
     }
 }
